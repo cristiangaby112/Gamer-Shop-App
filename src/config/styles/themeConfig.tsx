773aa43635ad355432ce1f7574ddcc7ad9fa185b
cristiangaby112 <cristiangabrielbarriento@outlook.com>
2022-07-11 22:59:38 -0300

    configuracion de themeConfig.

---
+++ src/config/styles/themeConfig.tsx
@@ -0,0 +1,17 @@
+import { createTheme } from '@mui/material/styles';
+
+const theme = createTheme({
+  palette:{
+    primary: {
+      light: "#ff7b02",
+      main: "#f36917",
+      dark: "#100f0d"
+    },
+    secondary: {
+      main: "#ffffff",
+    }
+
+  }
+});
+
+export default theme;
